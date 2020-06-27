23b401976daa679a2d81c8ab9c9e566f8ecc7ed1
1538993458@qq.com <1538993458@qq.com>
2020-06-27 23:20:04 +0800

    上传图片组件

---
+++ pages/works/childcomps/WorksLoad.vue
@@ -0,0 +1,30 @@
+<template>
+  <view>
+    <robbyImageUpload @add="renderImage" />
+    <view v-for="(item, index) in items" :key="index">
+    </view>
+  </view>
+</template>
+
+<script>
+import robbyImageUpload from "@/components/robby-image-upload/robby-image-upload.vue";
+
+export default {
+  components: {
+    robbyImageUpload
+  },
+  data() {
+    return {
+      renImage: [] //渲染的图片
+    };
+  },
+  methods: {
+    renderImage(imgArr) {
+      this.renImage = imgArr.currentImages;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
